511e45f9a215ab8e8e316762b0633b02f3a7b692
Arun BAarode <developer@mindcrawller.com>
2026-07-22 17:04:11 +0530

    changes

---
+++ src/utils/webSession.js
@@ -0,0 +1,31 @@
+const WEB_SESSION_MARKER = "mediconnectWebSession";
+
+const WEB_AUTH_KEYS = [
+  "accessToken",
+  "token",
+  "refreshToken",
+  "isAuthenticated",
+  "userRole",
+  "userType",
+  "userEmail",
+  "userData",
+  "user",
+  "userId",
+  "counsellorId",
+  "counselorId",
+  "isVerified",
+  "profilePhoto",
+  "fullName",
+  "name",
+];
+
+// sessionStorage survives reloads but is removed when the browser/tab session
+// ends. localStorage is still used by the existing web app during one session;
+// stale credentials are discarded before React or API clients can use them.
+export const initializeWebSession = () => {
+  if (sessionStorage.getItem(WEB_SESSION_MARKER)) return;
+
+  WEB_AUTH_KEYS.forEach((key) => localStorage.removeItem(key));
+  sessionStorage.setItem(WEB_SESSION_MARKER, String(Date.now()));
+};
+
